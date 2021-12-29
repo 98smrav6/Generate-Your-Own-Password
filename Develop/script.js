a7503c59c2330774e7b05f097071f5161066acfc
98smrav6 <89051027+98smrav6@users.noreply.github.com>
2021-12-28 20:10:01 -0500

    Generate button is live.

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,33 @@
 // Assignment code here
+function getPasswordOptions() {
+  var length = parseInt(
+    prompt('How many characters would you like your password to contain?')
+  );
 
+  if (Number.isNaN(length)) {
+    alert('Password length must be provided as a number');
+    return null;
+  }
 
+  if (length < 8) {
+    alert('Password length must be at least 8 characters');
+    return null;
+  }
+
+  if (length > 128) {
+    alert('Password length must be less than 129 characters');
+    return null;
+  }
+}
+
+function generatePassword() {
+  var options = getPasswordOptions();
+// Variable to store password as it's being concatenated
+  var result = [];
+
+// Arrray to store types of characters to include in password
+  var possibleCharacters = [];
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
